b7094bcbe3196a186e8250e9e777be36ff9ec115
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2024-02-28 09:55:39 +0100

    initial settings and refactoring

---
+++ src/main/java/com/stocktrading/dataconsumer1/EventTopic.java
@@ -0,0 +1,5 @@
+package com.stocktrading.dataconsumer1;
+
+public enum EventTopic {
+    STOCK_INFO
+}
--- src/main/java/com/stocktrading/dataconsumer1/KafkaListeners.java
+++ src/main/java/com/stocktrading/dataconsumer1/KafkaListeners.java
@@ -1,16 +1,21 @@
 package com.stocktrading.dataconsumer1;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import static com.stocktrading.dataconsumer1.EventTopic.STOCK_INFO;
+
 @Component
+@Log4j2
 public class KafkaListeners {
 
     @KafkaListener(
-            topics = "stockInfo",
+            topics = "STOCK_INFO",
             groupId = "foo"
     )
     void listener(String data) {
-        System.out.println("Listener received following data: " + data);
+        System.out.println("Listener received following data " + data);
+        log.info("Listener received following data: {}", data);
     }
 }
